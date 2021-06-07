e539fb7d1408a5cec168116afa7d799c96ae1b80
Christopher Day <christophersday@gmail.com>
2021-06-06 19:59:35 -0600

    Update the RunAnalysis Class
    Adjust the info to account for previous changes

--- src/main/java/org/matsim/velbert/analysis/RunAnalysis.java
+++ src/main/java/org/matsim/velbert/analysis/RunAnalysis.java
@@ -3,11 +3,13 @@ package org.matsim.velbert.analysis;
 import org.apache.commons.csv.CSVFormat;
 import org.matsim.core.events.EventsUtils;
 import org.matsim.core.events.handler.EventHandler;
+import org.matsim.api.core.v01.population.Person;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collection;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class RunAnalysis {
@@ -15,11 +17,17 @@ public class RunAnalysis {
     public static void main(String[] args) {
 
         var manager = EventsUtils.createEventsManager();
-        var handler = new ModalShareCalculator();
-        manager.addHandler(handler);
+        var homeHandler = new HomeCalculator();
+        var modeHandler = new ModalShareCalculator();
+        manager.addHandler(homeHandler);
+        manager.addHandler(modeHandler);
+
         EventsUtils.readEvents(manager, "D:\\Users\\chris\\Documents\\CSDSchool\\MatSimClass\\projects\\velbert\\scenarios\\velbert-v1.0-1pct\\cluster-outputs\\velbert-v1.0-1pct.output_events.xml.gz");
 
-        var personTrips = handler.getPersonTrips();
+        //var velbertPeeps = homeHandler.getVelbertPersons();
+        //var ppeeps = modeHandler.getPeeps();
+        var personTrips = modeHandler.getPersonTrips();
+
         var modes = personTrips.values().stream()
                 .flatMap(Collection::stream)
                 .collect(Collectors.toMap(mode -> mode, mode -> 1, Integer::sum));
@@ -38,7 +46,6 @@ public class RunAnalysis {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
 
     }
 
